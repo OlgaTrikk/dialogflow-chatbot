731e8f364e532abc54c865e157c4f16449bcb0b3
Olga Trikk <prymnesium13@gmail.com>
2022-01-02 12:15:36 +0200

    cleanup

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const http = require('http');
 // import env variables
 require('dotenv').config();
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.status(200).send('Server is working.')
@@ -18,11 +18,11 @@ app.post('/getmovie', (req, res) => {
 	const movieToSearch =
 		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
 			? req.body.queryResult.parameters.movie
-			: ''
+			: '';
 
 	const reqUrl = encodeURI(
 		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${process.env.API_KEY}`
-	)
+	);
 	http.get(
 		reqUrl,
 		responseFromAPI => {
@@ -60,48 +60,6 @@ app.post('/getmovie', (req, res) => {
 	)
 })
 
-
-// app.post('/themoviedb_getmovie', (req, res) => {
-// 	const movieToSearch =
-// 		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
-// 			? req.body.queryResult.parameters.movie
-// 			: '';      
-
-// 	const reqUrl = encodeURI(
-//         `http://api.themoviedb.org/3/search/movie?api_key=${process.env.THEMOVIEDB_API_KEY}&query=${movieToSearch}`
-// 	);
-//     console.log(reqUrl);
-// 	http.get(
-// 		reqUrl,
-// 		responseFromAPI => {
-// 			let completeResponse = ''
-// 			responseFromAPI.on('data', chunk => {
-// 				completeResponse += chunk
-// 			})
-// 			responseFromAPI.on('end', () => {
-// 				const response = JSON.parse(completeResponse);
-//                 const movie = response.results[0]
-
-// 				let dataToSend = movieToSearch;
-// 				dataToSend = `${movie.title} was released in the year ${movie.release_date}.
-//                 Here some glimpse of the plot: ${movie.overview}.
-//                 }`
-
-// 				return res.json({
-// 					fulfillmentText: dataToSend,
-// 					source: 'getmovie'
-// 				})
-// 			})
-// 		},
-// 		error => {
-// 			return res.json({
-// 				fulfillmentText: 'Could not get results at this time',
-// 				source: 'getmovie'
-// 			})
-// 		}
-// 	)
-// })
-
 app.listen(port, () => {
-	console.log(`🌏 Server is running at http://localhost:${port}`)
-})
+	console.log(`🌏 Server is running at http://localhost:${port}`);
+});
