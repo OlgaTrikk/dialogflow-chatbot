d831cbc252d7064f794bbc788b6ece936dc23f96
Olga Trikk <prymnesium13@gmail.com>
2022-01-02 10:22:36 +0200

    another movie db

--- index.js
+++ index.js
@@ -12,16 +12,57 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
 	res.status(200).send('Server is working.')
-})
+});
 
 app.post('/getmovie', (req, res) => {
+	const movieToSearch =
+		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+			? req.body.queryResult.parameters.movie
+			: ''
+
+	const reqUrl = encodeURI(
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${process.env.API_KEY}`
+	)
+	http.get(
+		reqUrl,
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				const movie = JSON.parse(completeResponse)
+
+				let dataToSend = movieToSearch
+				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+					movie.Director
+				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.
+                }`
+
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'getmovie'
+				})
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'getmovie'
+			})
+		}
+	)
+})
+
+
+app.post('/themoviedb_getmovie', (req, res) => {
 	const movieToSearch =
 		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
 			? req.body.queryResult.parameters.movie
 			: '';      
 
 	const reqUrl = encodeURI(
-        `http://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${movieToSearch}`
+        `http://api.themoviedb.org/3/search/movie?api_key=${process.env.THEMOVIEDB_API_KEY}&query=${movieToSearch}`
 	);
     console.log(reqUrl);
 	http.get(
